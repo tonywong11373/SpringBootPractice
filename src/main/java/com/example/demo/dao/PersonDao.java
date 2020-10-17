62a4637e86efd44159a89eadbbf6fea023969588
Tony Wong <tonywong11373@hotmail.com>
2020-10-18 03:37:25 +0800

    Updates 2020-10-18 03:37:24

--- src/main/java/com/example/demo/dao/PersonDao.java
+++ src/main/java/com/example/demo/dao/PersonDao.java
@@ -1,5 +1,15 @@
 package com.example.demo.dao;
 
+import java.util.UUID;
+
+// For the benefits of using Interface to allow Dependency Injection for the sake of 
+// conducting an Unit Testing Automated
 public interface PersonDao {
-    
+    int insertPerson(UUID id, Person person);
+
+    default int addPerson(Person person){
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+    // We have two methods, one allow insert person with id, one without id in which the id would be randomly generated
 }
