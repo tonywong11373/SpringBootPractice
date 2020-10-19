308ea0d3fdbf7348cf9d36f0b822736e1694befb
Tony Wong <tonywong11373@hotmail.com>
2020-10-19 16:28:13 +0800

    Updates 2020-10-19 16:28:13

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -38,6 +38,7 @@ public class PersonController {
         return personService.getAllPeople();
     }
 
+    @GetMapping(path = "{id}")
     public Person getPersonById(@PathVariable("id") UUID id)
     {
         return personService.getPersonById(id)
